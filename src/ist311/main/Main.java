e8e53824b34a0c69b48531783a0d2623e40bf761
Jason Nucciarone <nucci.programming@gmail.com>
2021-02-14 16:29:54 -0500

    Complete tests for PostRating

--- src/ist311/main/Main.java
+++ src/ist311/main/Main.java
@@ -4,9 +4,100 @@ package ist311.main;
 import ist311.usecases.PostRating;
 import ist311.usecases.SearchBusiness;
 
+import org.javatuples.Pair;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Main method to test that PostRating and SearchBusiness Use Cases work.
+ * @author Jason C. Nucciarone
+ *
+ */
 public class Main {
     public static void main(String[] args) {
-        String hello = "Hello World!";
-        System.out.println(hello);
+        /*
+          First, to test our ability to update the BUSINESS_REVIEW database.
+          Try to add two entries to separate collections.
+
+          Assumptions:
+          We will be assuming that the user has already written their review
+          and that we have retrieved the information from the user interface.
+          We will also be assuming that the database and relevant collections
+          are already defined.
+
+          The following fields will have been completed:
+            - user_id
+            - user_name
+            - business_id
+            - business_name
+            - user_safety_rating
+            - tags
+            - user_review
+         */
+
+        // Define assumptions
+        int user_id_1 = 1001;
+        Pair<String, String> user_name_1 = Pair.with("Kyle", "Montenucci");
+        int business_id_1 = 2002;
+        String business_name_1 = "MICHEALS_PIZZA_001";
+        double user_safety_rating_1 = 7.25;
+        List<String> tags_1 = new ArrayList<>();
+        tags_1.add("Not clean");
+        tags_1.add("Wearing masks wrong!");
+        String user_review_1 = "Just bad staff. I usually love this place! Wear masks correctly next time please.";
+
+        // Add to database
+        PostRating rating_1 = new PostRating(user_id_1,
+                user_name_1,
+                business_id_1,
+                business_name_1,
+                user_safety_rating_1,
+                tags_1,
+                user_review_1);
+        rating_1.updateCollection();
+
+        // Create another document
+        int user_id_2 = 1002;
+        Pair<String, String> user_name_2 = Pair.with("Sarah", "Conner");
+        int business_id_2 = 2002;
+        String business_name_2 = "MICHEALS_PIZZA_001";
+        double user_safety_rating_2 = 5;
+        List<String> tags_2 = new ArrayList<>();
+        tags_2.add("Not clean");
+        tags_2.add("No masks!");
+        tags_2.add("More the 6 feet");
+        String user_review_2 = "None of the kitchen staff were wearing masks, although, to give them credit they did " +
+                "have all the customers spaced out more than 6 feet!";
+
+        PostRating rating_2 = new PostRating(user_id_2,
+                user_name_2,
+                business_id_2,
+                business_name_2,
+                user_safety_rating_2,
+                tags_2,
+                user_review_2);
+        rating_2.updateCollection();
+
+        // Create document in a different collection
+        int user_id_3 = 1003;
+        Pair<String, String> user_name_3 = Pair.with("Kaylie", "Arujo");
+        int business_id_3 = 2003;
+        String business_name_3 = "VARLAS_THRIFT_SHOP_001";
+        double user_safety_rating_3 = 10;
+        List<String> tags_3 = new ArrayList<>();
+        tags_3.add("Free masks");
+        tags_3.add("Free hand sanitizer");
+        tags_3.add("Limit maximum occupancy");
+        String user_review_3 = "Place was an absolute dream!. I felt safer here than in my own apartment!";
+
+        PostRating rating_3 = new PostRating(user_id_3,
+                user_name_3,
+                business_id_3,
+                business_name_3,
+                user_safety_rating_3,
+                tags_3,
+                user_review_3);
+        rating_3.updateCollection();
+
     }
 }
